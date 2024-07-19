53b9b11988609bab731211df2bec301bb0673607
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-07-19 13:24:15 -0300

    [created] utils file

---
+++ utils.py
@@ -0,0 +1,18 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Start a server")
+    parser.add_argument(
+        "cards_per_player",
+        metavar="cards_per_player",
+        type=int,
+        help="The number of cards each player should have",
+    )
+    parser.add_argument(
+        "turns",
+        metavar="turns",
+        type=int,
+        help="The number of turns to play",
+    )
+    return parser.parse_args()
