2f47ee733f608ff2b02b7104f64e401d583b26b6
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-07-20 21:47:55 -0300

    [add] getters and docstrings

--- card.py
+++ card.py
@@ -1,8 +1,27 @@
 class Card:
     def __init__(self, suit, rank, value):
         self.suit = suit
+        """
+        suit: naipe da carta (♦️, ♠️, ❤️, ♣️)
+
+        """
         self.rank = rank
+        """
+        rank: valor da carta (2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A)
+        """
         self.value = value
+        """
+        value: valor numerico representando a "força" da carta
+        """
+
+    def get_suit(self):
+        return self.suit
+
+    def get_rank(self):
+        return self.rank
+
+    def get_value(self):
+        return self.value
 
     def __repr__(self):
         return f"{self.rank} {self.suit} ({self.value})"
