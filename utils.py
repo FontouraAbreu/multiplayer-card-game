1dde2d7b0997ba5b56c48ea52c527358b1be33ee
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-07-22 12:03:57 -0300

    [add] send message wrapper function

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+import sys
 
 
 def parse_args():
@@ -16,6 +18,32 @@ def parse_args():
         help="The number of turns to play",
     )
     return parser.parse_args()
+
+
+def send_message(conn, message: bytes):
+    """
+    Sends a message to the client and waits for an ACK response
+    while the message is not an ACK message it will keep sending the message
+    """
+    print("size of the filled message:", sys.getsizeof(message))
+    print("Sending message:", message)
+
+    conn.sendall(message)
+    # waits for ACK response
+    print("waiting for ACK")
+    answer = conn.recv(412)
+    answer = json.loads(answer.decode("utf-8"))
+    msg_type = answer["msg"]["type"]
+    # keep sending the message until an ACK is received
+    while msg_type != "ACK":
+        # send the message again
+        message = conn.sendall(message)
+        # receive the answer again
+        answer = conn.recv(sys.getsizeof(message))
+        answer = json.loads(answer.decode("utf-8"))
+        msg_type = answer["msg"]["type"]
+
+    print("ACK received")
 
 
 def calculate_crc8(message):
