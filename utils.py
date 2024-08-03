1d8e9fbe237077a1c1272b544ee5f8f5adcbe6cf
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-08-03 17:15:20 -0300

    [changed] sending and receiving data functions to use the recvfrom and sendto socket methods

--- utils.py
+++ utils.py
@@ -37,47 +37,46 @@ def parse_client_args():
     return parser.parse_args()
 
 
-def send_message(conn, message: bytes):
+def send_message(listen_socket, send_socket, message: bytes, address):
     """
     Sends a message to the client and waits for an ACK response
     while the message is not an ACK message it will keep sending the message
     """
     # print("size of the filled message:", sys.getsizeof(message))
     print("Sending message and waiting for ACK/NACK:", message)
-    print(conn)
+    print("Address:", address)
     try:
-        conn.sendall(message)
-    except Exception as e:
+        send_socket.sendto(message, address)
+    except socket.error as e:
         print("Error sending message:", e)
-        return
 
     # waits for ACK response
     print("waiting for ACK")
-    answer = conn.recv(RECV_BUFFER)
+    answer, addr = listen_socket.recvfrom(RECV_BUFFER)
     answer = json.loads(answer.decode("utf-8"))
     msg_type = answer["msg"]["type"]
     # keep sending the message until an ACK is received
     while msg_type != "ACK":
         # send the message again
         try:
-            message = conn.sendall(message)
+            send_socket.sendto(message, address)
         except Exception as e:
             print("Error sending message:", e)
             return
 
         # receive the answer again
-        answer = conn.recv(RECV_BUFFER)
+        answer, addr = listen_socket.recvfrom(RECV_BUFFER)
         answer = json.loads(answer.decode("utf-8"))
         msg_type = answer["msg"]["type"]
 
     print("ACK received")
 
 
-def receive_message(conn, player_id):
+def receive_message(listen_socket, send_socket, address):
     """
     Receives a message from the client and sends an ACK response
     """
-    message = conn.recv(RECV_BUFFER)
+    message, addr = listen_socket.recvfrom(RECV_BUFFER)
     if not message:
         return None
 
@@ -93,7 +92,7 @@ def receive_message(conn, player_id):
         answer_crc8["msg"]["type"] = "NACK"
         answer_crc8["crc8"] = 1
         try:
-            conn.sendall(json.dumps(answer_crc8).encode("utf-8"))
+            send_socket.sendto(json.dumps(answer_crc8).encode("utf-8"), address)
         except socket.error as e:
             print(f"Error sending message - {e}")
             time.sleep(5)
@@ -110,7 +109,7 @@ def receive_message(conn, player_id):
         answer_crc8["msg"]["type"] = "ACK"
         answer_crc8["crc8"] = 1
         try:
-            conn.sendall(json.dumps(answer_crc8).encode("utf-8"))
+            send_socket.sendto(json.dumps(answer_crc8).encode("utf-8"), address)
         except socket.error as e:
             print(f"Error sending message - {e}")
             time.sleep(5)
@@ -122,25 +121,35 @@ def receive_message(conn, player_id):
     return message
 
 
-def receive_message_no_ack(conn, player_id):
+def receive_message_no_ack(listen_sock, send_socket, player_id, next_node):
     """
     Receives a message from the client without sending an ACK response
     """
-    message = conn.recv(RECV_BUFFER)
+    message, addr = listen_sock.recvfrom(RECV_BUFFER)
     if not message:
         return None
+
+    print("Received message:", message)
 
     message = json.loads(message.decode("utf-8"))
 
     # check if the message is destined to the player
+    # if not, send to the next node
     if message["msg"]["dst"] != player_id:
-        print("Message not destined to player")
+        print("Message not destined to the player")
+        message = json.dumps(message, indent=2).encode("utf-8")
+        try:
+            send_socket.sendto(message, next_node)
+            print("Message sent to the next node: {}".format(next_node))
+        except socket.error as e:
+            print(f"Error sending message - {e}")
+            return None
         return -1
 
     return message
 
 
-def send_ack_or_nack(conn, message, current_player_id):
+def send_ack_or_nack(sock, message, current_player_id, address):
     """
     Sends an ACK or NACK message to the client
     """
@@ -169,7 +178,7 @@ def send_ack_or_nack(conn, message, current_player_id):
 
     # send the message to the next node
     try:
-        conn.sendall(json.dumps(ack_or_nack_answer).encode("utf-8"))
+        sock.sendto(json.dumps(ack_or_nack_answer).encode("utf-8"), address)
     except socket.error as e:
         print(f"Error sending message - {e}")
         time.sleep(5)
